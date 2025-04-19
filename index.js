e8bd583ea858a62338b182f79dfc85225f56c8b1
indexduo <83288588+IndexDuo@users.noreply.github.com>
2025-04-19 22:52:25 +0000

    Apr 19, 2025, 6:52 PM

--- index.js
+++ index.js
@@ -1,23 +1,25 @@
-const getPoemBtn = document.getElementById('get-poem')
-const poemEl = document.getElementById('poem')
-const poemURL = 'https://poetrydb.org/random,linecount/1;12/author,title,lines.json'
+const getPoemBtn = document.getElementById("get-poem");
+const poemEl = document.getElementById("poem");
+const poemURL =
+    "https://poetrydb.org/random,linecount/1;12/author,title,lines.json";
 
-const getJSON = url => fetch(url).then(res => res.json())
+const getJSON = (url) => fetch(url).then((res) => res.json());
 
-const pipe = (...fns) => firstArg => fns.reduce((returnValue, fn) => fn(returnValue), firstArg)
+const pipe =
+    (...fns) =>
+    (firstArg) =>
+        fns.reduce((returnValue, fn) => fn(returnValue), firstArg);
 
-const makeTag = tag => str => `<${tag}>${str}</${tag}>`
+const makeTag = (tag) => (str) => `<${tag}>${str}</${tag}>`;
 
 // complete this function
 const makePoemHTML = () => {
-
-//use of both makeTag and pipe at least once
-
-
-}
+    //use of both makeTag and pipe at least once
+    return "hi";
+};
 
 // attach a click event to #get-poem
-getPoemBtn.onclick = async function() {
-  // renders the HTML string returned by makePoemHTML to #poem
-  poemEl.innerHTML = makePoemHTML(await getJSON(poemURL))
-}
+getPoemBtn.onclick = async function () {
+    // renders the HTML string returned by makePoemHTML to #poem
+    poemEl.innerHTML = makePoemHTML(await getJSON(poemURL));
+};
